ea812687c9b69803926a955a4746f7b953827aba
Nixon Correa <ndncorrea@gmail.com>
2022-04-28 21:33:21 +0200

    FX_wakka

--- PACMAN GAME/Project_9_Solution/Source/ModulePlayer.cpp
+++ PACMAN GAME/Project_9_Solution/Source/ModulePlayer.cpp
@@ -64,6 +64,7 @@ bool ModulePlayer::Start()
 	texture = App->textures->Load("Assets/Sprites/Pac-Man/Pac-Man ripped.png");
 	currentAnimation = &idleAnim;
 
+	wakka = App->audio->LoadFx("Assets/Music/Fx/wakka.wav");
 	laserFx = App->audio->LoadFx("Assets/Fx/laser.wav");
 	explosionFx = App->audio->LoadFx("Assets/Fx/explosion.wav");
 
@@ -251,6 +252,7 @@ void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
 
 	if (c1->type == Collider::Type::PLAYER && c2->type == Collider::Type::SUPERPACDOT) {
 		score += 10;
+		App->audio->PlayFx(wakka, 5);
 	}
 }
 
