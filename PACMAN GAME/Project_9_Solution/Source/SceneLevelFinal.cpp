b770f41463bace2dd85efab1184e7112b28d129c
DaniMariagesUPC <99950347+DaniMariages@users.noreply.github.com>
2022-06-06 17:52:52 +0200

    FinalLevel Tileset

--- PACMAN GAME/Project_9_Solution/Source/SceneLevelFinal.cpp
+++ PACMAN GAME/Project_9_Solution/Source/SceneLevelFinal.cpp
@@ -30,7 +30,7 @@ bool SceneLevelFinal::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/Sprites/background_level_4.png");
+	bgTexture = App->textures->Load("Assets/Sprites/Final_Level.png");
 
 	extraTexture = App->textures->Load("Assets/Sprites/Extras_background.png");
 
